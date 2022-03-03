1f6836af90d2d037b74a64939808108bed1261c0
Cocowwy <514658459@qq.com>
2022-03-03 22:09:01 +0800

    初始化项目结构

---
+++ showdb-test/src/main/java/cn/cocowwy/showdbtest/ShowdbTestApplication.java
@@ -0,0 +1,13 @@
+package cn.cocowwy.showdbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowdbTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowdbTestApplication.class, args);
+    }
+
+}
---
+++ showdb-test/src/test/java/cn/cocowwy/showdbtest/ShowdbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cocowwy.showdbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowdbTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
