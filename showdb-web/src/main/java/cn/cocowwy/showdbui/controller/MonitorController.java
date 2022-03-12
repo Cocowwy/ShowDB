5c75bc981e555e93a581f339c59b09c354dc78ba
Cocowwy <514658459@qq.com>
2022-03-12 19:51:54 +0800

    add controller

---
+++ showdb-web/src/main/java/cn/cocowwy/showdbui/controller/MonitorController.java
@@ -0,0 +1,11 @@
+package cn.cocowwy.showdbui.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Cocowwy
+ * @create 2022-03-03-19:01
+ */
+@RestController("/monitor")
+public class MonitorController {
+}
---
+++ showdb-web/src/main/java/cn/cocowwy/showdbui/controller/StructController.java
@@ -0,0 +1,11 @@
+package cn.cocowwy.showdbui.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Cocowwy
+ * @create 2022-03-03-18:58
+ */
+@RestController("/showdb/struct")
+public class StructController {
+}
