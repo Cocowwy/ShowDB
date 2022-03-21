dd2b64326d501c215112e9df02f4f0262c1440bd
Cocowwy <514658459@qq.com>
2022-03-21 22:11:24 +0800

    add error defind

--- showdb-core/src/main/java/cn/cocowwy/showdbcore/exception/ErrorDefinition.java
+++ showdb-core/src/main/java/cn/cocowwy/showdbcore/exception/ErrorDefinition.java
@@ -6,4 +6,5 @@ package cn.cocowwy.showdbcore.exception;
  */
 public class ErrorDefinition {
     public static final Error UNUSE_ENDPOINT = new Error(100, "该功能未启用");
+    public static final Error NOT_SUPPORTED = new Error(101, "暂不支持当前功能~");
 }
