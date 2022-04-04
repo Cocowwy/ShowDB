45fbb0dbdc0cab92001072680857e62634705e20
Cocowwy <514658459@qq.com>
2022-04-04 20:00:16 +0800

    add mock test

--- showdb-core/src/main/java/cn/cocowwy/showdbcore/strategy/impl/mysql/MySqlMonitorExecuteStrategy.java
+++ showdb-core/src/main/java/cn/cocowwy/showdbcore/strategy/impl/mysql/MySqlMonitorExecuteStrategy.java
@@ -58,6 +58,24 @@ public class MySqlMonitorExecuteStrategy implements MonitorExecuteStrategy, MySq
             ss.setMasterLogFile(rs.getString("Master_Log_File"));
             return ss;
         });
+
+        // todo mock test data
+        if (ds.equals("oms")) {
+            SlaveStatus mock = new SlaveStatus();
+            mock.setSlaveIOState("Waiting for master to send event");
+            mock.setMasterHost("localhost");
+            mock.setMasterUser("photorepl");
+            mock.setMasterPort("3301");
+            mock.setMasterRetryCount("86400");
+            mock.setSlaveIORunning("Yes");
+            mock.setSlaveSQLRunning("Yes");
+            mock.setSqlDelay("0");
+            mock.setRelayLogFile("mysqld-relay-bin.005201");
+            mock.setMasterLogFile("mysql-bin.001822");
+            return mock;
+        }
+        // ------------mock---------------
+
         return CollectionUtils.lastElement(ipCounts);
     }
 
