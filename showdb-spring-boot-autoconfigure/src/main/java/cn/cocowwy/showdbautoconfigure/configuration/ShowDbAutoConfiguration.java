588c4e385a2f3837c82cbaeb767328a82ef8adf6
Cocowwy <514658459@qq.com>
2022-04-06 21:57:11 +0800

    tiny upd

--- showdb-spring-boot-autoconfigure/src/main/java/cn/cocowwy/showdbautoconfigure/configuration/ShowDbAutoConfiguration.java
+++ showdb-spring-boot-autoconfigure/src/main/java/cn/cocowwy/showdbautoconfigure/configuration/ShowDbAutoConfiguration.java
@@ -22,7 +22,6 @@ import org.springframework.util.CollectionUtils;
 import javax.sql.DataSource;
 import java.sql.SQLException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
