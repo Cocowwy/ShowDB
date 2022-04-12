cf5b0929e27594179b50112188aa1093e32699fc
Cocowwy <514658459@qq.com>
2022-04-12 22:22:21 +0800

    取Innodb锁等待超时时间

--- showdb-core/src/main/java/cn/cocowwy/showdbcore/strategy/impl/mysql/MySqlConfigExecuteStrategy.java
+++ showdb-core/src/main/java/cn/cocowwy/showdbcore/strategy/impl/mysql/MySqlConfigExecuteStrategy.java
@@ -1,8 +1,6 @@
 package cn.cocowwy.showdbcore.strategy.impl.mysql;
 
-import cn.cocowwy.showdbcore.cache.ShowDbCache;
 import cn.cocowwy.showdbcore.config.ShowDbFactory;
-import cn.cocowwy.showdbcore.entities.MySqlVariables;
 import cn.cocowwy.showdbcore.strategy.ConfigExecuteStrategy;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
@@ -38,23 +36,39 @@ public class MySqlConfigExecuteStrategy implements ConfigExecuteStrategy, MySqlE
         return CollectionUtils.lastElement(rts);
     }
 
-    /**
-     * mysql参数详情
-     */
-    public MySqlVariables variables(String ds) {
-        String key = ShowDbCache.buildCacheKey(ds, "variables", ds);
+//    /**
+//     * mysql参数详情
+//     */
+//    public MySqlVariables variables(String ds) {
+//        String key = ShowDbCache.buildCacheKey(ds, "variables", ds);
+//
+//        ShowDbCache.cache().computeIfAbsent(key, (k) -> {
+//            String sql = "show variables where Variable_name\n" +
+//                    "in ('version_compile_os',\n" +
+//                    "    'autocommit',\n" +
+//                    "    'version', 'basedir',\n" +
+//                    "    'default_storage_engine',\n" +
+//                    "   'transaction_isolation',\n" +
+//                    "   'innodb_lock_wait_timeout');\n";
+//            ShowDbFactory.getJdbcTemplate(ds).query(sql, (rs, i) -> {
+//                MySqlVariables variable = new MySqlVariables();
+//                variable.setVersionCompileOs(rs.getString("version_compile_os"));
+//                return null;
+//            }
+//        });
+//        return null;
+//    }
 
-        ShowDbCache.cache().computeIfAbsent(key, (k) -> {
-            String sql = "show variables where Variable_name\n" +
-                    "in ('version_compile_os',\n" +
-                    "    'autocommit',\n" +
-                    "    'version', 'basedir',\n" +
-                    "    'default_storage_engine',\n" +
-                    "   'transaction_isolation',\n" +
-                    "   'innodb_lock_wait_timeout');\n";
-            return null;
-        });
-        return null;
+    /**
+     * 获取Innodb锁等待超时时间
+     * @param ds
+     * @return 超时时间
+     */
+    @Override
+    public String innodbLockWaitTimeout(String ds) {
+        String sql = "show variables like 'innodb_lock_wait_timeout'";
+        List<String> rts = ShowDbFactory.getJdbcTemplate(ds).query(sql, (rs, i) -> rs.getString("Value"));
+        return CollectionUtils.lastElement(rts);
     }
 
     /**
