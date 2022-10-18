9609e037eec152879ddde493cab3d71e599abb1e
Cocowwy <514658459@qq.com>
2022-10-18 17:38:22 +0800

    👾 新增SpringDataJDBC的方法实现（空）

---
+++ showdb-core/src/main/java/cn/cocowwy/showdbcore/generate/impl/SpringDataJDBCGeneratorImpl.java
@@ -0,0 +1,15 @@
+package cn.cocowwy.showdbcore.generate.impl;
+
+import cn.cocowwy.showdbcore.entities.GenerateDefind;
+import cn.cocowwy.showdbcore.generate.GeneratorService;
+
+/**
+ * @author cocowwy.cn
+ * @create 2022-05-05-11:45
+ */
+public class SpringDataJDBCGeneratorImpl implements GeneratorService {
+    @Override
+    public void generate(String ds, String tableName, GenerateDefind generateDefind) throws Exception {
+
+    }
+}
